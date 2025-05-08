789adaa55ac062e0cbb7bcfd676ae30b215bb07e
Aaron Bandado <aaron.aminu1@gmail.com>
2025-05-08 22:52:08 +0300

    feat: update middleware to redirect authenticated users from the login and register pages

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,31 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from '@/utils/supabase/middleware'
+import { NextResponse, type NextRequest } from "next/server";
+import { updateSession } from "@/utils/supabase/middleware";
+import { createClient } from "./utils/supabase/server";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  try {
+    const supabase = await createClient();
+
+    const {
+      data: { user },
+      error,
+    } = await supabase.auth.getUser();
+
+    const authRoutes = ["/login", "/register"].includes(
+      request.nextUrl.pathname
+    );
+
+    if (user && authRoutes) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    if (error) {
+      console.error("Supabase auth error:", error.message);
+    }
+  } catch (error) {
+    console.error("Middleware error", error);
+  }
+  return await updateSession(request);
 }
 
 export const config = {
@@ -14,6 +37,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
-}
+};

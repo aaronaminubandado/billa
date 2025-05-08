0c4523c0bc5d1295c9323f0ff2ffa9e756d93f7f
Aaron Bandado <aaron.aminu1@gmail.com>
2025-05-08 21:39:59 +0300

    refactor: added return statements to login server action

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -6,24 +6,24 @@ import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
 
 export async function login(formData: FormData) {
-  const supabase = await createClient()
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
+    const supabase = await createClient();
+  
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+  
+    const { error } = await supabase.auth.signInWithPassword({ email, password });
+  
+    if (error) {
+      return { success: false, error: error.message };
+    }
+  
+   
+    revalidatePath('/', 'layout');
+  
+    
+    return { success: true };
   }
-
-  const { error } = await supabase.auth.signInWithPassword(data)
-
-  if (error) {
-    redirect('/error')
-  }
-
-  revalidatePath('/', 'layout')
-  redirect('/')
-}
+  
 
 export async function signup(formData: FormData) {
   const supabase = await createClient()

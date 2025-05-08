ac9e0096972c6606c66bd29ce8dbd899286e2e3e
Aaron Bandado <aaron.aminu1@gmail.com>
2025-05-08 23:00:25 +0300

    chore: remove useless comments

--- lib/validation.ts
+++ lib/validation.ts
@@ -8,12 +8,11 @@ export const loginSchema = z.object({
 export type LoginFormData = z.infer<typeof loginSchema>;
 
 
-// 🧠 Define the schema using Zod
 export const formSchema = z.object({
   username: z.string().min(3, "Username must be at least 3 characters."),
   email: z.string().email("Invalid email address."),
   password: z.string().min(8, "Password must be at least 8 characters."),
 });
 
-// 🧾 Infer the form type from the schema
+
 export type SignupFormData = z.infer<typeof formSchema>;
